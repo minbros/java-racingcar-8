a48625fd5ce719f4695557ef1dba4b385414e92e
minbros <pristo24631@gmail.com>
2025-10-23 20:53:54 +0900

    test: view 계층 단위 테스트 작성

---
+++ src/test/java/racingcar/view/ConsoleViewTest.java
@@ -0,0 +1,76 @@
+package racingcar.view;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ConsoleViewTest {
+    private ConsoleView view;
+    private final InputStream originalIn = System.in;
+    private final PrintStream originalOut = System.out;
+    private ByteArrayOutputStream outContent;
+
+    @BeforeEach
+    void setUp() {
+        view = new ConsoleView();
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setIn(originalIn);
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void 유저_입력_받기() {
+        String input = "hi,min\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        String result = view.read();
+
+        assertThat(result).isEqualTo("hi,min");
+    }
+
+    @Test
+    void 게임_진행_출력() {
+        Car car1 = new Car("min", 2);
+        Car car2 = new Car("bros", 3);
+
+        view.printProgress(List.of(car1, car2));
+
+        String output = outContent.toString();
+        assertThat(output).contains("min : --", "bros : ---");
+    }
+
+    @Test
+    void 게임_우승자_출력() {
+        Car winningCar = new Car("min", 5);
+
+        view.printResult(List.of(winningCar));
+
+        String output = outContent.toString();
+        assertThat(output).contains("최종 우승자 : min");
+    }
+
+    @Test
+    void 게임_공동_우승자_출력() {
+        Car winningCar1 = new Car("min", 5);
+        Car winningCar2 = new Car("bros", 5);
+
+        view.printResult(List.of(winningCar1, winningCar2));
+
+        String output = outContent.toString();
+        assertThat(output).contains("최종 우승자 : min, bros");
+    }
+}
