df2c181f9044807fde979dcb953c33129746ad58
minbros <pristo24631@gmail.com>
2025-10-23 21:14:32 +0900

    feat: 횟수 입력받기 기능 구현

--- src/main/java/racingcar/view/ConsoleView.java
+++ src/main/java/racingcar/view/ConsoleView.java
@@ -8,9 +8,15 @@ import java.util.List;
 public class ConsoleView {
     private static final String PROMPT_FOR_NAMES =
             "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분";
+    private static final String PROMPT_FOR_COUNT = "시도할 횟수는 몇 회인가요?";
 
     public String readCarNames() {
         System.out.println(PROMPT_FOR_NAMES);
+        return Console.readLine();
+    }
+
+    public String readCount() {
+        System.out.println(PROMPT_FOR_COUNT);
         return Console.readLine();
     }
 
