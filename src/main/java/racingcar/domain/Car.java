44084ab2d7cffc152a4a346c00b79e1d804719e2
minbros <pristo24631@gmail.com>
2025-10-23 19:58:55 +0900

    feat: 도메인 계층 스텁 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move() {
+    }
+}
