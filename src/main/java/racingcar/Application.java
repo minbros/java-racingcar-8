64322595a65319976bcffd05cc82e6c7c4907e34
minbros <pristo24631@gmail.com>
2025-10-23 19:59:06 +0900

    feat: 메인 클래스 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.service.RacingService;
+import racingcar.view.ConsoleView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ConsoleView consoleView = new ConsoleView();
+        RacingService racingService = new RacingService();
+        RacingController racingController = new RacingController(consoleView, racingService);
+        racingController.run();
     }
 }
