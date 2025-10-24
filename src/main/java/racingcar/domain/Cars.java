7fd1d783c0f658ec4b1d95f32c1f4a00bb5b4511
minbros <pristo24631@gmail.com>
2025-10-25 00:53:34 +0900

    feat: cars 이름 중복 검증 로직 변경

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -3,17 +3,20 @@ package racingcar.domain;
 import racingcar.constant.ErrorMessage;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Cars {
     private final List<Car> carList = new ArrayList<>();
+    private final Set<String> names = new HashSet<>();
 
     public void add(Car car) {
-        List<String> names = carList.stream().map(Car::getName).toList();
         if (names.contains(car.getName())) {
             throw new IllegalArgumentException(ErrorMessage.DUPLICATE_CAR_NAMES.getMessage());
         }
         carList.add(car);
+        names.add(car.getName());
     }
 
     public List<Car> get() {
