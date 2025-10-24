8073b0af4973b9ee3af25548f4d5345b469c9935
minbros <pristo24631@gmail.com>
2025-10-24 22:55:32 +0900

    test: 자동차 추가 단위 테스트 생성

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,17 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> carList = new ArrayList<>();
+
+    public void add(Car car) {
+
+    }
+
+    public List<Car> getCars() {
+        return Collections.emptyList();
+    }
+}
---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,17 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarsTest {
+    @Test
+    void 자동차_정상_추가_확인() {
+        Cars cars = new Cars();
+        Car car = new Car("min", 1);
+
+        cars.add(car);
+
+        assertThat(cars.getCars()).contains(car);
+    }
+}
