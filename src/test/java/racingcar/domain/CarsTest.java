2190f42f7498cb4f9e8676082588d0db4ff5cfa1
minbros <pristo24631@gmail.com>
2025-10-24 23:05:14 +0900

    test: 자동차 추가 시 이름 중복 검증 테스트 추가

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,8 +1,10 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.Test;
+import racingcar.constant.ErrorMessage;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarsTest {
     @Test
@@ -13,5 +15,18 @@ class CarsTest {
         cars.add(car);
 
         assertThat(cars.get()).contains(car);
+    }
+
+    @Test
+    void 자동차_추가_에러_확인() {
+        Cars cars = new Cars();
+        Car car1 = new Car("min", 1);
+        Car car2 = new Car("min", 3);
+
+        cars.add(car1);
+
+        assertThatThrownBy(() -> cars.add(car2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.DUPLICATE_CAR_NAMES.getMessage());
     }
 }
