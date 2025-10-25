f3bace110842d0b919153c9951c00fab1c615dd9
minbros <pristo24631@gmail.com>
2025-10-25 16:41:22 +0900

    test: 횟수 파싱 예외 테스트 추가

--- src/test/java/racingcar/controller/InputParserTest.java
+++ src/test/java/racingcar/controller/InputParserTest.java
@@ -1,10 +1,14 @@
 package racingcar.controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.constant.ErrorMessage;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputParserTest {
     @Test
@@ -23,5 +27,13 @@ class InputParserTest {
         int count = InputParser.parseCount(input);
 
         assertThat(count).isEqualTo(10);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"two", "2.4", "100000000000"})
+    void 횟수_파싱_예외_확인(String input) {
+        assertThatThrownBy(() -> InputParser.parseCount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.COUNT_IS_NOT_IN_INT_RANGE.getMessage());
     }
 }
