01dd4ec25e82c775f8b80ec031722bf544baa0bb
minbros <pristo24631@gmail.com>
2025-10-25 22:37:43 +0900

    feat: 컨트롤러 기능 구현

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,7 +1,11 @@
 package racingcar.controller;
 
+import racingcar.domain.Car;
 import racingcar.service.RacingService;
+import racingcar.util.InputParser;
 import racingcar.view.ConsoleView;
+
+import java.util.List;
 
 public class RacingController {
     private final ConsoleView view;
@@ -13,6 +17,35 @@ public class RacingController {
     }
 
     public void run() {
+        List<String> names = getCarNamesFromUser();
+        int count = getTryCountFromUser();
+        registerCars(names);
+        playRounds(count);
+        printWinners();
+    }
 
+    private List<String> getCarNamesFromUser() {
+        String namesInput = view.readCarNames();
+        return InputParser.parseNames(namesInput);
+    }
+
+    private int getTryCountFromUser() {
+        String countInput = view.readCount();
+        return InputParser.parseCount(countInput);
+    }
+
+    private void registerCars(List<String> names) {
+        names.forEach(name -> service.addCar(new Car(name)));
+    }
+
+    private void playRounds(int count) {
+        for (int i = 0; i < count; i++) {
+            service.playOneRound();
+            view.printProgress(service.getCars());
+        }
+    }
+
+    private void printWinners() {
+        view.printResult(service.getWinningCars());
     }
 }
