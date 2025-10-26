a7a1fac3006dff56eae1a32b273ee4752ee7617f
minbros <pristo24631@gmail.com>
2025-10-27 00:52:37 +0900

    docs: InputParser 클래스에 주석 추가

--- src/main/java/racingcar/util/InputParser.java
+++ src/main/java/racingcar/util/InputParser.java
@@ -5,22 +5,49 @@ import racingcar.constant.ErrorMessage;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 유저 입력을 검증하는 유틸리티 클래스입니다.
+ */
 public final class InputParser {
     private InputParser() {
     }
 
+    // 이름 구분자
+    private static final String NAME_SEPARATOR = ",";
+
+    /**
+     * 입력받은 문자열을 {@value #NAME_SEPARATOR}를 기준으로 구분합니다.
+     * <p>이름 양 옆의 공백은 모두 지워집니다.</p>
+     *
+     * @return 구분된 이름 리스트
+     * @throws IllegalArgumentException 이름의 구분이 잘못된 경우<br>
+     *                                  예시: ",min,bros" 또는 "min,bros," 또는 "min,,bros"
+     */
     public static List<String> parseNames(String input) {
-        List<String> names = Arrays.stream(input.split(",", -1))
+        List<String> names = Arrays.stream(input.split(NAME_SEPARATOR, -1))
                 .map(String::trim)
                 .toList();
 
-        if (names.stream().anyMatch(String::isBlank)) {
+        if (names.stream().anyMatch(String::isEmpty)) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_NAME_FORMAT.getMessage());
         }
 
         return names;
     }
 
+    /**
+     * 입력받은 문자열을 경기 횟수로 변환합니다.
+     * <p>횟수 양 옆의 공백은 모두 지워집니다.</p>
+     *
+     * @return 정수로 변환된 경기 횟수
+     * @throws IllegalArgumentException 입력받은 횟수가 다음과 같은 경우
+     *                                  <ul>
+     *                                      <li>양수가 아닌 경우</li>
+     *                                      <li>int 범위를 벗어날 경우</li>
+     *                                      <li>소수일 경우</li>
+     *                                      <li>"two"와 같이 숫자를 사용하지 않은 표현일 경우</li>
+     *                                  </ul>
+     */
     public static int parseCount(String input) {
         String trimmedInput = input.trim();
         try {
