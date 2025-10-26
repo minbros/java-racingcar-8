25351628c2077333264e738082786394cd3e694c
minbros <pristo24631@gmail.com>
2025-10-27 01:12:20 +0900

    docs: ConsoleView 클래스에 주석 추가

--- src/main/java/racingcar/view/ConsoleView.java
+++ src/main/java/racingcar/view/ConsoleView.java
@@ -5,6 +5,9 @@ import racingcar.domain.Car;
 
 import java.util.List;
 
+/**
+ * 사용자의 입력을 받거나 게임 진행 상황을 출력하는 클래스입니다.
+ */
 public class ConsoleView {
     private static final String PROMPT_FOR_NAMES =
             "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
@@ -20,10 +23,24 @@ public class ConsoleView {
         return Console.readLine();
     }
 
+    /**
+     * 게임이 시작될 때, 가독성을 높이기 위해 사용자의 입력과 게임 진행 상황을 분리합니다.
+     */
     public void printStart() {
         System.out.println("\n실행 결과");
     }
 
+    /**
+     * 각 자동차마다 현재 위치를 포매팅하여 출력합니다.
+     * <p>시행 횟수마다 빈 줄로 분리되어 출력합니다.</p>
+     * 출력 예시
+     * <pre>
+     * min : ---
+     * bros : --
+     * </pre>
+     *
+     * @param cars 전체 자동차 리스트
+     */
     public void printProgress(List<Car> cars) {
         for (Car car : cars) {
             String progress = getProgress(car);
@@ -32,6 +49,14 @@ public class ConsoleView {
         System.out.println();
     }
 
+    /**
+     * 최종 우승 자동차의 이름을 포매팅하여 출력합니다.
+     * <p>공동 우승자가 존재할 경우, ", "로 구분되어 출력합니다.</p>
+     * <p>
+     * 출력 예시: {@code 최종 우승자 : min, bros}
+     *
+     * @param winningCars 우승한 자동차 리스트
+     */
     public void printResult(List<Car> winningCars) {
         String message = "최종 우승자 : ";
         List<String> winningNames = winningCars.stream().map(Car::getName).toList();
