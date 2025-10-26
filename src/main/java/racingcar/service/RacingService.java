875e2e365cc44bc308d0205b44a11181c6934a5e
minbros <pristo24631@gmail.com>
2025-10-26 23:43:06 +0900

    docs: RacingService 클래스에 주석 추가

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -6,6 +6,11 @@ import racingcar.domain.Cars;
 
 import java.util.List;
 
+/**
+ * 자동차 경주 게임 로직을 담당하는 클래스입니다.
+ * <li>cars: 경주에 이용되는 모든 자동차 담당</li>
+ * @see Cars
+ */
 public class RacingService {
     private final Cars cars;
 
@@ -13,10 +18,21 @@ public class RacingService {
         this.cars = cars;
     }
 
+    /**
+     * 한 회만큼 자동차 경주를 진행합니다.
+     * <p>
+     *     자동차 개수만큼의 랜덤 값을 생성해 자동차의 이동에 사용합니다.
+     * </p>
+     * @see Cars#moveAll(List)
+     */
     public void playOneRound() {
         cars.moveAll(generateRandomValues());
     }
 
+    /**
+     * @throws IllegalArgumentException 자동차의 이름이 중복되는 경우
+     * @see Cars#add(Car)
+     */
     public void addCar(Car car) {
         cars.add(car);
     }
